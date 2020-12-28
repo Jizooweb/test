296b240c696e82681368e3d8d59be65366fa2112
Jizooweb <alex.grecu.daniel@gmail.com>
2020-12-28 12:38:30 -0800

    first commit

---
+++ packages/ui/README.md
@@ -0,0 +1,11 @@
+# `@test/ui`
+
+> TODO: description
+
+## Usage
+
+```
+const ui = require('@test/ui');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/ui/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UIComponent = () => {
+return (
+<h1>Scriu cum vreau!</h1>
+);
+};
+
+export default UIComponent;
